4a77757277c59d29773a686a6e1c5b3391269b49
Sangmeebee <apfhdznzl@gmail.com>
2022-08-28 22:28:17 +0900

    feat: GetMovieUseCaseTest 테스트 코드 구현

--- domain/src/test/java/com/sangmeebee/searchmovie/domain/usecase/GetMovieUseCaseTest.kt
+++ domain/src/test/java/com/sangmeebee/searchmovie/domain/usecase/GetMovieUseCaseTest.kt
@@ -1,16 +1,32 @@
 package com.sangmeebee.searchmovie.domain.usecase
 
+import com.sangmeebee.searchmovie.domain.repository.MovieRepository
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
 class GetMovieUseCaseTest {
 
+    private lateinit var getMovieUseCase: GetMovieUseCase
+    private lateinit var movieRepository: MovieRepository
+
     @Before
     fun setUp() {
+        movieRepository = mockk(relaxed = true)
+        getMovieUseCase = GetMovieUseCase(movieRepository)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `영화 정보를 불러온다`() {
-        TODO("Not yet implemented")
+    fun `영화 정보를 불러온다`() = runTest {
+
+        //when
+        getMovieUseCase("")
+
+        //then
+        coVerify { movieRepository.getMovies("") }
     }
 }
