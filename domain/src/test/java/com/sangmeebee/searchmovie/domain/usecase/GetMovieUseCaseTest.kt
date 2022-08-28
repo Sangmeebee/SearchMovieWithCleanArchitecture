e09a169f4b5bd0cabda00874d1dbe0981dae246b
Sangmeebee <apfhdznzl@gmail.com>
2022-08-28 22:22:49 +0900

    feat: GetMovieUseCaseTest test code 세팅

---
+++ domain/src/test/java/com/sangmeebee/searchmovie/domain/usecase/GetMovieUseCaseTest.kt
@@ -0,0 +1,16 @@
+package com.sangmeebee.searchmovie.domain.usecase
+
+import org.junit.Before
+import org.junit.Test
+
+class GetMovieUseCaseTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun `영화 정보를 불러온다`() {
+        TODO("Not yet implemented")
+    }
+}
