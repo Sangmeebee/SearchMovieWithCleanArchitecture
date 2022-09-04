9efa65ea0b089c6b16d732128385ba76b1ba4b4e
Sangmeebee <apfhdznzl@gmail.com>
2022-09-04 20:13:16 +0900

    feat: data module을 paging library를 사용하기 위해 kotlin library에서 android library로 전환

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,11 +1,37 @@
 plugins {
-    id("kotlin")
+    id("com.android.library")
+    id("kotlin-android")
     id("kotlin-kapt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["runnerBuilder"] =
+            "de.mannodermaus.junit5.AndroidJUnit5Builder"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
