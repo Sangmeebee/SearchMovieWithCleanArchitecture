70b782b9a30f1bc0e79b8b737fa0de76b65857d8
Sangmeebee <apfhdznzl@gmail.com>
2022-09-11 03:03:28 +0900

    feat: editText bindingadapter로 onActionDone 추가

--- presentation/src/main/java/com/sangmeebee/searchmovie/presentation/util/BindingAdpaters.kt
+++ presentation/src/main/java/com/sangmeebee/searchmovie/presentation/util/BindingAdpaters.kt
@@ -1,5 +1,7 @@
 package com.sangmeebee.searchmovie.presentation.util
 
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
@@ -16,4 +18,18 @@ fun ImageView.setImageByUrl(url: String) {
 @BindingAdapter("htmlText")
 fun TextView.setHtmlText(text: String) {
     setText(HtmlCompat.fromHtml(text, HtmlCompat.FROM_HTML_MODE_LEGACY))
+}
+
+@BindingAdapter("onActionDone")
+fun EditText.setOnActionDone(onActionDone: (String) -> Unit) {
+    setOnEditorActionListener { _, actionId, _ ->
+        return@setOnEditorActionListener when (actionId) {
+            EditorInfo.IME_ACTION_DONE -> {
+                onActionDone(text.toString())
+                true
+            }
+            else -> false
+
+        }
+    }
 }
