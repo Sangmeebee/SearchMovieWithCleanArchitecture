97c236434c319202e3eaaba827505df0342ca5f2
Sangmeebee <apfhdznzl@gmail.com>
2022-09-21 23:17:56 +0900

    feat: minsdk 24로 마이그레이션

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sangmeebee.searchmovie"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
