b7120c6c5a0b2a7f1a55fa7e2dc70736640dec73
Sangmeebee <apfhdznzl@gmail.com>
2022-10-02 12:33:30 +0900

    feat: NonSignInFragment 화면 추가

---
+++ app/src/main/java/com/sangmeebee/searchmovie/ui/my/NonSignInFragment.kt
@@ -0,0 +1,8 @@
+package com.sangmeebee.searchmovie.ui.my
+
+import com.sangmeebee.searchmovie.databinding.FragmentNonSigninBinding
+import com.sangmeebee.searchmovie.ui.base.BaseFragment
+
+class NonSignInFragment :
+    BaseFragment<FragmentNonSigninBinding>(FragmentNonSigninBinding::inflate) {
+}
